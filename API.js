308e916dcc8e40c5441ac1ec38e5ae0eab7bac1a
maserati520 <nishejagladys@gmail.com>
2026-05-22 16:42:26 +0200

    API

---
+++ API.js
@@ -0,0 +1,14 @@
+function fetchPosts() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            return data;
+        })
+        .catch((error) =>{
+            console.error('Error fetching posts:', error);
+             return [];
+        });
+
+}
+fetchPosts();
